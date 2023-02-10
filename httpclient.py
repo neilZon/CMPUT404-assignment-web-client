b3c59c98b8839e4d8587b0050a2e7af4f2110cf4
Neilzon <neilcviloria@gmail.com>
2023-02-09 21:33:09 -0700

    kidz bop

--- httpclient.py
+++ httpclient.py
@@ -22,7 +22,7 @@ import sys
 import socket
 import re
 # you may use urllib to encode data appropriately
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlencode
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -36,24 +36,8 @@ class HTTPClient(object):
     def __init__(self):
         self.socket = None
 
-    def url_encode(self, s):
-        return s.replace("\r", "%0D").replace("\n", "%0A")
-
     def build_post_body_args(self, args):
-        arg_list = []
-        for k, v in args.items():
-            k = self.url_encode(k)
-            v = self.url_encode(v)
-            if len(arg_list) > 0:
-                arg_list.append("&")
-                arg_list.append(k)
-                arg_list.append("=")
-                arg_list.append(v)  
-            else:
-                arg_list.append(k)
-                arg_list.append("=")
-                arg_list.append(v)
-        return "".join(arg_list)
+        return urlencode(args)
 
     def get_host_port(self, url):
         parsed_url = urlparse(url)
@@ -66,18 +50,16 @@ class HTTPClient(object):
         elif parsed_url.scheme == "http":
             port = 80
 
-        # print("##JK#JK#JKJ#K#JK#J", parsed_url.hostname, parsed_url.netloc, )
-
         return parsed_url.hostname, port, parsed_url.netloc
 
     def get_path(self, url):
         parsed_url = urlparse(url)
+        if (parsed_url.path is None) or (len(parsed_url.path) == 0):
+            return '/'
         return parsed_url.path
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if host != '127.0.0.1':
-            host = socket.gethostbyname(host)
         self.socket.connect((host, port))
         return None
 
@@ -108,18 +90,25 @@ class HTTPClient(object):
             if (part):
                 buffer.extend(part)
             else:
-                done = True
+                done = not part
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
         host, port, netloc = self.get_host_port(url)
         path = self.get_path(url)
 
+        print("sdjflksd", path, url)
+
         self.connect(host, port)
 
-        self.sendall(f"GET {path} HTTP/1.1\r\nHost: {netloc}\r\nUser-Agent: Mozilla/5.0\r\n\r\n")
-
-        self.socket.shutdown(socket.SHUT_WR)
+        self.sendall(
+            f"GET {path} HTTP/1.1\r\n" +
+            f"Host: {netloc}\r\n" +
+            "User-Agent: Mozilla/5.0\r\n" +
+            "Accept-Charset: UTF-8\r\n" +
+            "Accept: */*\r\n" +
+            "Connection: close\r\n\r\n"
+            )
 
         response = self.recvall(self.socket)
 
@@ -128,13 +117,13 @@ class HTTPClient(object):
         code = self.get_code(response)
         headers = self.get_headers(response_lines)
         body = self.get_body(response_lines)
-        # print(code, headers, body)
 
+        print(body)
         self.close()
         return HTTPResponse(code, body)
     
     def build_post_header_string(self, data):
-        return f"Content-Length: {len(data)}\r\nContent-Type: application/x-www-form-urlencoded\r\n"                
+        return f"Content-Length: {len(data)}\r\nContent-Type: application/x-www-form-urlencoded"                
 
     def POST(self, url, args=None):
         host, port, netloc = self.get_host_port(url)
@@ -148,9 +137,17 @@ class HTTPClient(object):
 
         req_headers = self.build_post_header_string(post_body)
 
-        self.sendall(f"POST {path} HTTP/1.1\r\nHost: {netloc}\r\nUser-Agent: Mozilla/5.0{req_headers}\r\n{post_body}")
-
-        self.socket.shutdown(socket.SHUT_WR)
+        self.sendall(
+            f"POST {path} HTTP/1.1\r\n" +
+            "Host: {netloc}\r\n" + 
+            "User-Agent: Mozilla/5.0\r\n" + 
+            "Accept-Charset: UTF-8\r\n" +
+            "Accept: */*\r\n" +
+            "Connection: close\r\n" +
+            f"{req_headers}\r\n" +
+            "\r\n" + 
+            f"{post_body}"
+            )
 
         response = self.recvall(self.socket)
 
